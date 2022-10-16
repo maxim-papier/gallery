84df66e676c6858e672aad6944969e437d83c2f3
Maxim Papier <109724501+maxim-papier@users.noreply.github.com>
2022-10-16 07:22:52 +0600

    Initial Commit

---
+++ Gallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gallery
+//
+//  Created by Maxim V. Brykov on 16.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
