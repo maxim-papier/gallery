433c32dbe256645c263fd03da04f59a6424e714f
Maxim Papier <maxim-paper@yandex.ru>
2022-11-09 07:37:25 +0600

    + layout works (table, image preview, textlabel and favourites iicon

--- Gallery/ImagesList/ImagesListViewController.swift
+++ Gallery/ImagesList/ImagesListViewController.swift
@@ -4,10 +4,12 @@ class ImagesListViewController: UIViewController {
     
     @IBOutlet private var tableView: UITableView!
     
+    private var photosName = [String]()
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        photosName = Array(0..<20).map{ "\($0)" }
     }
     
 }
@@ -27,8 +29,19 @@ extension ImagesListViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
+        
+        guard let imageListCell = cell as? ImagesListCell else {
+            return UITableViewCell()
+        }
+        
+        configCell(for: imageListCell, with: indexPath)
+        return imageListCell
     }
     
+    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
+        
+    }
     
 }
