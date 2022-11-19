b84a72e993ceee02440e1779bf4161ba42828981
Maxim Papier <maxim-paper@yandex.ru>
2022-11-19 17:20:34 +0600

    +navigation-controller-segue

--- Gallery/SingleView/SingleImageViewController.swift
+++ Gallery/SingleView/SingleImageViewController.swift
@@ -2,5 +2,17 @@ import UIKit
 
 class SingleImageViewController: UIViewController {
     
+    var image: UIImage! {
+        didSet {
+            guard isViewLoaded else { return }
+            imageView.image = image
+        }
+    }
     @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = image
+    }
+    
 }
