d3954d3fde9aa7feb95d27fd5678e70cb5f7c5fc
Maxim Papier <maxim-paper@yandex.ru>
2022-11-20 18:31:23 +0600

    + scrollSettining func

--- Gallery/SingleView/SingleImageViewController.swift
+++ Gallery/SingleView/SingleImageViewController.swift
@@ -6,17 +6,36 @@ class SingleImageViewController: UIViewController {
         didSet {
             guard isViewLoaded else { return }
             imageView.image = image
+            zoomSettings()
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.image = image
+        
     }
     
+    @IBOutlet var scrollView: UIScrollView!
     @IBOutlet var imageView: UIImageView!
     @IBAction func didTapBackwardButton() {
         dismiss(animated: true)
     }
     
 }
+
+// MARK: - EXTENTIONS
+
+extension SingleImageViewController {
+    func zoomSettings() {
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
+    }
+}
+
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+}
+
