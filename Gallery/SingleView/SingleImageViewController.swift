ae190ba74594894f7859eaa6a774bf5526473256
Maxim Papier <maxim-paper@yandex.ru>
2022-11-20 19:46:50 +0600

    + Rescale algorythm

--- Gallery/SingleView/SingleImageViewController.swift
+++ Gallery/SingleView/SingleImageViewController.swift
@@ -6,14 +6,15 @@ class SingleImageViewController: UIViewController {
         didSet {
             guard isViewLoaded else { return }
             imageView.image = image
-            zoomSettings()
+            rescaleAndCenterImageInScrollView(image: image)
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.image = image
-        
+        zoomSettings()
+        rescaleAndCenterImageInScrollView(image: image)
     }
     
     @IBOutlet var scrollView: UIScrollView!
@@ -26,16 +27,55 @@ class SingleImageViewController: UIViewController {
 
 // MARK: - EXTENTIONS
 
+extension SingleImageViewController: UIScrollViewDelegate {
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+}
+
+
 extension SingleImageViewController {
+    
     func zoomSettings() {
         scrollView.minimumZoomScale = 0.1
         scrollView.maximumZoomScale = 1.25
     }
 }
 
-extension SingleImageViewController: UIScrollViewDelegate {
-    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
-        return imageView
+
+// Rescale algorithm
+
+extension SingleImageViewController {
+    
+    private func rescaleAndCenterImageInScrollView(image: UIImage) {
+        
+        let minZoomScale = scrollView.minimumZoomScale
+        let maxZoomScale = scrollView.maximumZoomScale
+        
+        view.layoutIfNeeded() // Не понял зачем это здесь ???
+        
+        let visibleRectSize = scrollView.bounds.size
+        let imageSize = image.size
+        
+        // Вычисляю соотношение сторон
+        let hScale = visibleRectSize.width / imageSize.width
+        let vScale = visibleRectSize.height / imageSize.height
+        
+        // Нахожу значение зума для того, чтобы картинка была на весь экран
+        let scale = min(maxZoomScale, max(minZoomScale, max(hScale, vScale)))
+        
+        scrollView.setZoomScale(scale, animated: false)
+        
+        scrollView.layoutIfNeeded() // Принудительный пересчёт
+        
+        // Определяю contentOffset для картинки
+        let newContentSize = scrollView.contentSize
+        let x = (newContentSize.width - visibleRectSize.width) / 2
+        let y = (newContentSize.height - visibleRectSize.height) / 2
+        scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
+        
     }
+    
 }
 
