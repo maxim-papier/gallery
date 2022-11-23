ec390fadf6a14bd8df2b724042a5e575b3a14923
Maxim Papier <maxim-paper@yandex.ru>
2022-11-23 08:35:07 +0600

    + constants file

---
+++ Gallery/Constants.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+let accessKey = "45V_47NAE3YXOsxEma47rlcGz8W9gJQKwM68gqIGcjo"
+let secretKey = "Iuk0CV-2rMm4MSaTUvQ3UzPfWb1xdCsPHpe9u-FjUTc"
+let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let accessScope = "public+read_user+write_likes"
+let defaultBaseURL = URL(string: "https://api.unsplash.com/photos")
