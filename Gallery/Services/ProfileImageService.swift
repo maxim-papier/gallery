a1449576372f0fc6d9069fcb41617eaa8a64ae6a
Maxim Papier <maxim-paper@yandex.ru>
2023-01-19 13:34:05 +0600

    + ProfileService refactored + ProfileImage refactored

--- Gallery/Services/ProfileImageService.swift
+++ Gallery/Services/ProfileImageService.swift
@@ -2,85 +2,48 @@ import Foundation
 
 final class ProfileImageService {
     
-    static let shared = ProfileImageService() // Singleton
-    static let DidChangeNotification = Notification.Name(rawValue: "ProfileImageProviderDidChange") // Notification name
-    
+    private let session = URLSession.shared
+    private var task: URLSessionTask?
     private(set) var avatarURL: String?
     
-    let urlSession = URLSession.shared
-    var task: URLSessionTask?
-    let token = OAuth2TokenStorage().token
-    
+    static let shared = ProfileImageService() // Singleton
+    static let DidChangeNotification = Notification.Name(rawValue: "ProfileImageProviderDidChange") // Notification name
+
+    private let tokenStorage = OAuth2TokenStorage()
     
     func fetchProfileImageURL(username: String, _ completion: @escaping (Result<String, Error>) -> Void) {
         
-        var request = URLRequest.makeHTTPRequest(
+        let request = URLRequest.makeHTTPRequest(
             path: "/user/\(username)",
             httpMethod: "GET",
             baseURL: K.defaultBaseAPIURL
         )
         
-        guard let token = token else {
+        guard tokenStorage.token != nil else {
             completion(.failure(TokenStorageError.tokenNotFound))
             return
         }
-        request.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
         
-        let task = urlSession.dataTask(with: request) { data, response, error in
-            if let error = error {
+        task = session.objectTask(for: request) { [weak self] (result: Result<UserResult, Error>) in
+            guard let self = self else { return }
+            
+            switch result {
+            
+            case .success(let profileImageResult):
+                
+                guard let avatarURL = profileImageResult.profileImage.small else {
+                    completion(.failure(FetchProfileImageError.noImageDataFound))
+                    return
+                }
+                self.avatarURL = avatarURL
+                completion(.success(avatarURL))
+                
+                NotificationCenter.default.post(name: ProfileImageService.DidChangeNotification, object: self, userInfo: ["URL": avatarURL])
+            
+            case .failure(let error):
                 completion(.failure(error))
-            } else {
-                self.handleResponse(response, data: data, completion: completion)
             }
         }
-        task.resume()
-        
     }
-    
-    
-    private func handleResponse(_ response: URLResponse?, data: Data?, completion: @escaping (Result<String, Error>) -> Void) {
-        
-        guard let response = response as? HTTPURLResponse, (200...299).contains(response.statusCode) else {
-            DispatchQueue.main.async {
-                completion(.failure(FetchProfileImageError.invalidResponse))
-                return
-            }
-            return
-        }
-        handleData(data, completion: completion)
-        
-    }
-    
-    
-    private func handleData(_ data: Data?, completion: @escaping (Result<String, Error>) -> Void) {
-        
-        if let data {
-            decodeData(data, completion: completion)
-        } else {
-            completion(.failure(FetchProfileImageError.dataError))
-            return
-        }
-        
-    }
-    
-    
-    private func decodeData(_ data: Data, completion: @escaping (Result<String, Error>) -> Void) {
-        
-        let decoder = JSONDecoder()
-        
-        guard let profileImageResult = try? decoder.decode(UserResult.self, from: data) else {
-            completion(.failure(FetchProfileImageError.decodingData))
-            return
-        }
-        
-        guard let avatarURL = profileImageResult.profileImage.small else {
-            completion(.failure(FetchProfileImageError.noImageDataFound))
-            return
-        }
-        
-        completion(.success(avatarURL))
-        NotificationCenter.default.post(name: ProfileImageService.DidChangeNotification, object: self, userInfo: ["URL": avatarURL]) // Publish notification
-        
-    }
-    
+
 }
