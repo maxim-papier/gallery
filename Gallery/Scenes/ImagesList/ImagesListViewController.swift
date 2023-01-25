ae77ff92024db6caecdbea52078a12cbe56a96a7
Maxim Papier <maxim-paper@yandex.ru>
2023-01-25 19:43:00 +0600

    + Observer methods

--- Gallery/Scenes/ImagesList/ImagesListViewController.swift
+++ Gallery/Scenes/ImagesList/ImagesListViewController.swift
@@ -25,9 +25,20 @@ class ImagesListViewController: UIViewController {
         tableView.register(ImagesListCell.self, forCellReuseIdentifier: "\(ImagesListCell.self)")
         
         tableView.delegate = self
-        tableView.dataSource = self        
+        tableView.dataSource = self
         
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        observeImagesListChanges()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        stopObservingImagesListChanges()
+    }
+    
     
     
     // When user tap on the cell
@@ -75,7 +86,7 @@ extension ImagesListViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
+                
         let cell = tableView.dequeueReusableCell(withIdentifier: "\(ImagesListCell.self)" , for: indexPath)
         
         guard let imagesListCell = cell as? ImagesListCell else {
@@ -83,8 +94,19 @@ extension ImagesListViewController: UITableViewDataSource {
         }
         
         let photo = imagesListService.photos[indexPath.row]
+        let url = photo.thumbnailImage
+        //imagesListCell.imageView?.kf.setImage(with: photo.thumbnailImage)
+        imagesListCell.previewImage.kf.setImage(with: url)
         
-        imagesListCell.imageView?.kf.setImage(with: photo.thumbnailImage)
+        /*
+         guard let currentPhotoName = UIImage(
+             named: photoNames[indexPath.row]) else { return }
+         
+         cell.previewImage.image = currentPhotoName
+         cell.dateLabel.text = dateFormatter.string(from: Date())
+         
+
+         */
         
         return imagesListCell
     }
@@ -106,6 +128,21 @@ extension ImagesListViewController: UITableViewDataSource {
 
 extension ImagesListViewController {
     
+    private func observeImagesListChanges() {
+           imagesListObserver = notificationCenter.addObserver(
+               forName: imagesListService.didChangeNotification,
+               object: nil,
+               queue: .main
+           ) { [weak self] _ in
+               self?.updateTableViewAnimated()
+           }
+       }
+
+       private func stopObservingImagesListChanges() {
+           if let imagesListObserver {
+               notificationCenter.removeObserver(imagesListObserver)
+           }
+       }
     private func updateTableViewAnimated() {
         
         let oldCount = tableView.numberOfRows(inSection: 0)
@@ -115,6 +152,8 @@ extension ImagesListViewController {
             
             let newIndexPath = (oldCount..<newCount).map { IndexPath(row: $0, section: 0)
             }
+            
+            print("performBatchUpdates!!!")
             
             tableView.performBatchUpdates {
                 tableView.insertRows(at: newIndexPath, with: .automatic)
